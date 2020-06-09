090d59e1eab13e91fa6b86ac40221cb885648811
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-06-09 15:22:34 -0700

    add error handling to user input
    Kevin Davis <thekidnamedkd@gmail.com>

--- scripts.js
+++ scripts.js
@@ -1,16 +1,26 @@
-$( document ).ready(function() {
+$(document).ready(function() {
   let originalInput = prompt("Enter a word or sentence and we'll tell you if it's a palindrome");
+  var noSpaceInput = originalInput.replace(/[" "\|&;\$%@"?'-<>\(\)\+,/]/g, "");
+  console.log(noSpaceInput); // var cleanString = dirtyString.replace(/[\|&;\$%@"<>\(\)\+,]/g, "");
 
   function reverser(input) {
     let newInput = input.split("").reverse().join("");
     console.log(newInput);
 
-    if (originalInput === newInput) {
+    if (noSpaceInput === newInput) {
       alert("this is a palindrome")
     } else {
       alert('this is not a palindrome')
     }
   }
 
-  reverser(originalInput);
+  reverser(noSpaceInput);
 });
+
+//var cleanString = dirtyString.replace(/[|&;$%@"<>()+,]/g, "");
+
+// function isPalindrome(s,i) {
+//  return(i=i||0)<0||i>=s.length>>1||s[i]==s[s.length-1-i]&&isPalindrome(s,++i);
+// }
+// console.log(isPalindrome('test'));
+// console.log(isPalindrome('racecar'));
